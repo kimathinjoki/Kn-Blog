13471411f3a9f04fe3029ca13ba14306141f56b4
kimathinjoki <dfkimathinjoki@gmail.com>
2023-05-08 23:47:27 -0700

    test file

--- test.js
+++ test.js
@@ -10,4 +10,19 @@ const greet = (name)=>{
 greet("Brani")
 
 
-console.log(global)
+// console.log(global)
+
+
+setTimeout(()=>{
+    console.log("in 5 I will  clear interval")
+    clearInterval(int)
+},5000)
+
+
+const int = setInterval(()=>{
+    console.log("Hello World")
+}, 1000)
+
+
+console.log(__dirname)
+console.log(__filename)
