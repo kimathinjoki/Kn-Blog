e8f08058c124ce2210d622bd06ac5bc669949957
kimathinjoki <dfkimathinjoki@gmail.com>
2023-05-08 23:47:19 -0700

    add people file

---
+++ people.js
@@ -0,0 +1,14 @@
+ const  people = ["name", "believe", "yourself", "equal"]
+ const ages = [34,56,78,23,67,22]
+
+ console.log(people)
+
+
+ module.exports = {
+    people, ages
+}
+
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir())
