1540549cb750b2f48069d031a6bec4d669d67c7d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-05-09 01:37:15 -0700

    server prac

---
+++ server.js
@@ -0,0 +1,69 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res)=>{
+    // const io = require('socket.io')
+    console.log(req.url, req.method)
+    
+
+    // set header content type
+
+    res.setHeader('Content-Type', 'text/html');
+
+
+    let path = './views'
+
+
+    switch(req.url){
+        case '/':
+            path += '/index.html'
+            res.statusCode = 200
+
+            break;
+        case '/about':
+            path += '/about.html'
+            res.statusCode = 200
+
+            break;
+        case '/about-me':
+            // path += '/about.html'
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+            break;
+        default:
+            path += '/404.html'
+            res.statusCode = 404
+
+            break;
+
+    }
+
+
+
+    // send a html file
+
+    fs.readFile(path,(err, data)=>{
+        if(err){
+            console.log(err)
+            res.end()
+        }else{
+            // res.write(data)
+            res.end(data)
+        }
+    })
+
+})
+
+server.listen(3000,
+            res.end(data)
+    }
+    })
+
+
+
+})
+
+server.listen(3001, 'localhost',() => {
+    console.log('listening for request on port 3000')
+})
