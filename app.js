26ca35eaa354b2eee740e511f4fb4ae3b824c1f4
kimathinjoki <dfkimathinjoki@gmail.com>
2023-05-09 22:13:17 -0700

    add middleware

--- app.js
+++ app.js
@@ -1,35 +1,77 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan')
 
 
 //  express app
 
 const app = express()
 
+//  register view engine
+
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews')
+
 //  listen for requests
 app.listen(3001);
 
+
+
+//  middleware & static files
+
+// app.use((req,res, next)=>{
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+//     next()
+// })
+
+
+// app.use((req,res, next)=>{
+//     console.log("on to the next one")
+//     next()
+// })
+
+app.use(express.static('public'))
+
+
+app.use(morgan('dev'))
+
 app.get('/', (req,res)=>{
     // res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html',{root: __dirname})
+    
+    // res.sendFile('./views/index.html',{root: __dirname})
+
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+
+    res.render('index',{ title: 'Home', blogs})
 
 })
 
 app.get('/about', (req,res)=>{
     // res.send('')
-    res.sendFile('./views/about.html',{root: __dirname})
+    // res.sendFile('./views/about.html',{root: __dirname})
+    res.render('about', {title: 'About'})
 })
 
-// app.get('/', (req,res)=>{
-//     res.send('<p>Home page</p>')
-// })
+
+
+
+app.get('/blogs/create', (req,res)=>{
+    res.render('create', {title: 'Create a New Blog'})
+})
 
 
 
 // redirects
 
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about')
-})
+// app.get('/about-us', (req, res)=>{
+//     res.redirect('/about')
+// })
 
 
 //  404 page the position of this matters, it has to be at the bottom as it goes through every single request then executes
@@ -37,6 +79,6 @@ app.get('/about-us', (req, res)=>{
 
 
 app.use((req,res)=>{
-    res.status(404).res.sendFile('./views/404.html',{root: __dirname})
+    res.status(404).res.render('404', {title: '404'})
 
 })
