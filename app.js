024046d433edeefc60cf27225191687f76ecf53b
kimathinjoki <dfkimathinjoki@gmail.com>
2023-05-10 10:25:57 -0700

    add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const morgan = require('morgan')
-
+const mongoose = require('mongoose')
 
 //  express app
 
 const app = express()
+
+// connection to mongo db
+
+const dbURI = "mongodb+srv://dfkimathinjoki:1234@cluster0.bnqyrmj.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(dbURI)
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 //  register view engine
 
@@ -12,7 +22,7 @@ app.set('view engine', 'ejs');
 // app.set('views', 'myviews')
 
 //  listen for requests
-app.listen(3001);
+// app.listen(3001);
 
 
 
